b7894867d1e3a4b0c2dfa24aeaad35a451aa8f35
VIKAS SINGH <xanderbilla@gmail.com>
2023-06-22 00:20:23 +0530

    #Responsive Complete

--- src/components/ToggleMenu.jsx
+++ src/components/ToggleMenu.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import styles from '../styles/toggleMenu.module.css'
 import Sidebar from './Sidebar';
+import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
+import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
+import DeliveryDiningOutlinedIcon from '@mui/icons-material/DeliveryDiningOutlined';
+import { Link } from 'react-router-dom';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
 
 const ToggleMenu = ({ user }) => {
     const [isOpen, setIsOpen] = useState(false)
-
+console.log(user)
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -17,17 +22,22 @@ const ToggleMenu = ({ user }) => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.toggleButton}>
-                <div className={styles.hamburger} onClick={handleClick}>
-                    <span className={`${styles.lines} ${isOpen ? styles.open : ''}`}></span>
-                    <span className={`${styles.lines} ${isOpen ? styles.hide : ''}`}></span>
-                    <span className={`${styles.lines} ${isOpen ? styles.close : ''}`}></span>
-                </div>
-            </div>
-            <div className={`${styles.cart} ${user ? styles.hide : ''}`}>
+            <Link to='/cart'><div className={`${styles.cart} ${user ? styles.hide : ''}`}>
                 <img src="/img/cart.png" alt="" width="30" height="30" />
                 <div className={styles.counter}>2</div>
-            </div>
+            </div></Link>
+            <Link to='/menu'><div className={`${styles.cart}`}>
+                <RestaurantOutlinedIcon fontSize='large' style={{color:'white'}}/>
+            </div></Link>
+            <Link to='/contact'><div className={`${styles.cart}`}>
+                <ContactSupportOutlinedIcon fontSize='large' style={{color:'white'}}/>
+            </div></Link>
+            <Link to='/order'><div className={`${styles.cart} ${user ? styles.hide : ''}`}>
+                <DeliveryDiningOutlinedIcon fontSize='large' style={{color:'white'}}/>
+            </div></Link>
+            <Link to='/admin/dashboard'><div className={`${styles.cart}`}>
+                <ManageAccountsOutlinedIcon fontSize='large' style={{color:'white'}}/>
+            </div></Link>
             {
                 isOpen && <Sidebar user={user} isOpen={isOpen} setIsOpen={setIsOpen}/>
             }
